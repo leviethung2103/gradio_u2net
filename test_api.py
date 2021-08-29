38138c5dc00422768ce3b61ad9492e70b1b8dbe0
hunglv3 <hunglv@greenglobal.vn>
2021-08-29 18:48:13 +0700

    update crop function

--- test_api.py
+++ test_api.py
@@ -1,8 +1,10 @@
-# Author: Hung Le Viet 
-# This file is used to server the U2_Net Segmentation Model 
+# Author: Hung Le Viet
+# This file is used to server the U2_Net Segmentation Model
 # Utillize the FastAPI to host the application
-# I changed the paddlehub model -> native model 
+# I changed the paddlehub model -> native model
 # This is an upgrade version from api.py file
+
+# https://blog.csdn.net/wa1tzy/article/details/107015923
 
 import sys
 from fastapi import FastAPI, File, UploadFile, Form
@@ -15,98 +17,76 @@ import numpy as np
 import io
 import base64
 import cv2
-import os 
+import os
 import torch
-import time 
+import time
 from decouple import config
 from datetime import datetime
 from u2net.u2net_model import U2NET
-from u2net.processor import Processor
-from torch.autograd import Variable
+from u2net.processor import ObjectSegmentation
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import JSONResponse
 
 # FAST API
 app = FastAPI()
+app.mount("/output", StaticFiles(directory="output"), name="output")
 
-# Load the model 
+# Load the model
 seg_model = U2NET(3, 1)
+# model_dir = os.path.join(os.getcwd(), "u2net", "weights", "u2net_bce.pth")
+# model_dir = os.path.join(os.getcwd(), "u2net", "weights", "u2net_human_seg.pth")
+# model_dir = os.path.join(os.getcwd(), "u2net", "weights", "u2net_portrait.pth")
 model_dir = os.path.join(os.getcwd(), "u2net", "weights", "u2net.pth")
 
+API_HOST = config("API_HOST", default="0.0.0.0")
+API_PORT = config("API_PORT", default=5000, cast=int)
+
 if torch.cuda.is_available():
-    print ("Using the GPU")
-    print (torch.cuda.current_device())
-    print (torch.cuda.get_device_name(0))
+    print("Using the GPU")
+    print(torch.cuda.current_device())
+    print(torch.cuda.get_device_name(0))
     seg_model.load_state_dict(torch.load(model_dir))
     seg_model.cuda()
 else:
-    print ("[*] Using the CPU")
+    print("[*] Using the CPU")
     seg_model.load_state_dict(torch.load(model_dir, map_location="cpu"))
 seg_model.eval()
 
+# init only once
+object_segmentation = ObjectSegmentation(model=seg_model, batch_size=1, input_size=320)
 
-def predict(model,input_datas):
-    outputs = []
-    for data in input_datas:
-        # convert numpy array to torch Float Tensor
-        data = torch.from_numpy(data)
-        # change the data type
-        data = data.type(torch.FloatTensor)
-        if torch.cuda.is_available():
-            data = Variable(data.cuda())
-        else:
-            data = Variable(data)
-
-        # ! replace the paddle with torch
-        d1, d2, d3, d4, d5, d6, d7 = model(data)
-
-        # CUDA tensor -> host memory first -> convert to numpy
-        outputs.append(d1.detach().cpu().numpy())
-
-    outputs = np.concatenate(outputs, 0)
-
-    return outputs
-
-def Segmentation(images=None,
-                    paths=None,
-                    batch_size=1,
-                    input_size=320,
-                    output_dir='output',
-                    visualization=False):
-
-    # 初始化数据处理器
-    processor = Processor(paths, images, batch_size, input_size)
-
-    # 模型预测
-    outputs = predict(seg_model,processor.input_datas)
-
-    # 预测结果后处理
-    results = processor.postprocess(outputs, visualization=visualization, output_dir=output_dir)
-
-    return results
 
 class ImageSegmentation(BaseModel):
     image: str
 
+
 @app.post("/api/image-segmentation/from-file")
-async def image_segmentation_from_file(file: UploadFile = File(...)):
+async def image_segmentation_from_file(file: UploadFile = File(...), crop: bool = Form(...)):
     _start_time = time.time()
     fimage = await file.read()
     image = np.frombuffer(fimage, np.uint8)
     image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-    # -> image is BGR 
+    # -> image is BGR
 
-    # inference
-    result = Segmentation(
-        images=[image], paths=None, batch_size=1, input_size=320, output_dir="output", visualization=False
-    )
-    # foreground = result[0]["front"][:, :, ::-1]
+    # load data, preprocess, prediction, postprocess
+    object_segmentation.load_datas(paths=None, images=[image])
+    object_segmentation.preprocess()
+    outputs = object_segmentation.predict()
+    result = object_segmentation.postprocess(outputs, True, "output")
+
     foreground = result[0]["front"]
     mask = result[0]["mask"]
+    trans = result[0]["trans"]
+    crop_trans = result[0]["crop_trans"]
 
-    res, im_png = cv2.imencode(".png", foreground)
+    print(f"{datetime.now()} - Processing Time: {time.time() - _start_time}")
 
-    print (f"{datetime.now()} - Processing Time: {time.time() - _start_time}")
-
-    return StreamingResponse(io.BytesIO(im_png.tobytes()), media_type="image/png")
+    if crop:
+        ret_url = f"http://{API_HOST}:{API_PORT}/{crop_trans}"
+        return JSONResponse(status_code=200, content={"image": ret_url})
+    else:
+        ret_url = f"http://{API_HOST}:{API_PORT}/{trans}"
+        return JSONResponse(status_code=200, content={"image": ret_url})
 
 
 @app.post("/api/image-segmentation/from-base64")
@@ -115,17 +95,18 @@ async def image_segmentation_from_json(input: ImageSegmentation):
     image = base64.b64decode(p2s_request["image"])
     image = cv2.imdecode(np.fromstring(image, np.uint8), cv2.IMREAD_ANYCOLOR)
 
-    # inference
-    result = Segmentation(
-        images=[image], paths=None, batch_size=1, input_size=320, output_dir="output", visualization=False
-    )
-    foreground = result[0]["front"][:, :, ::-1]
+    # load data, preprocess, prediction, postprocess
+    object_segmentation.load_datas(paths=None, images=[image])
+    object_segmentation.preprocess()
+    outputs = object_segmentation.predict()
+    result = object_segmentation.postprocess(outputs, False, "output")
+
+    foreground = result[0]["front"]
     mask = result[0]["mask"]
 
-    res, im_png = cv2.imencode(".png", result)
+    res, im_png = cv2.imencode(".png", foreground)
     return StreamingResponse(io.BytesIO(im_png.tobytes()), media_type="image/png")
 
+
 if __name__ == "__main__":
-    API_HOST = config("API_HOST", default="0.0.0.0")
-    API_PORT = config("API_PORT",default=5000,cast=int)
     uvicorn.run(app, host=API_HOST, port=API_PORT)
--- u2net/processor.py
+++ u2net/processor.py
@@ -1,23 +1,33 @@
 import os
 import cv2
 import numpy as np
+import torch
+from torch.autograd import Variable
+import datetime
+from PIL import Image
 
-__all__ = ['Processor']
+__all__ = ["Processor"]
 
 
-class Processor():
-    def __init__(self, paths, images, batch_size, input_size):
-        # 图像列表
-        self.imgs = self.load_datas(paths, images)
+class ObjectSegmentation:
+    def __init__(self, model, batch_size, input_size):
+        # model
+        self.model = model
 
-        # 输入数据
-        self.input_datas = self.preprocess(self.imgs, batch_size, input_size)
+        # image list
+        self.imgs = None
 
-    # 读取数据函数
+        self.input_size = input_size
+        self.batch_size = batch_size
+
+        # input data
+        self.input_datas = None
+
+    # read data function
     def load_datas(self, paths, images):
         datas = []
 
-        # 读取数据列表
+        # read data list
         if paths is not None:
             for im_path in paths:
                 assert os.path.isfile(im_path), "The {} isn't a valid file path.".format(im_path)
@@ -27,14 +37,13 @@ class Processor():
         if images is not None:
             datas = images
 
-        # 返回数据列表
-        return datas
+        self.imgs = datas
 
-    # 预处理
-    def preprocess(self, imgs, batch_size=1, input_size=320):
+    # pre-processing image
+    def preprocess(self):
         input_datas = []
-        for image in imgs:
-            image = cv2.resize(image, (input_size, input_size))
+        for image in self.imgs:
+            image = cv2.resize(image, (self.input_size, self.input_size))
             tmpImg = np.zeros((image.shape[0], image.shape[1], 3))
             image = image / np.max(image)
 
@@ -50,11 +59,13 @@ class Processor():
         input_datas = np.concatenate(input_datas, 0)
 
         datas_num = input_datas.shape[0]
-        split_num = datas_num // batch_size + 1 if datas_num % batch_size != 0 else datas_num // batch_size
+        split_num = (
+            datas_num // self.batch_size + 1 if datas_num % self.batch_size != 0 else datas_num // self.batch_size
+        )
 
         input_datas = np.array_split(input_datas, split_num)
 
-        return input_datas
+        self.input_datas = input_datas
 
     def normPRED(self, d):
         ma = np.max(d)
@@ -64,8 +75,8 @@ class Processor():
 
         return dn
 
-    # 后处理
-    def postprocess(self, outputs, visualization=False, output_dir='output'):
+    # post-processing image
+    def postprocess(self, outputs, visualization=False, output_dir="output"):
         results = []
         if visualization and not os.path.exists(output_dir):
             os.mkdir(output_dir)
@@ -75,19 +86,143 @@ class Processor():
             pred = outputs[i, 0, :, :]
 
             pred = self.normPRED(pred)
+            # range of pixels ~ 0-> 1
+
+            # 320 x 320
+            print("Pred Shape:", pred.shape)
 
             # convert torch tensor to numpy array
             h, w = image.shape[:2]
-            mask = cv2.resize(pred, (w, h))
+            mask = cv2.resize(pred, (w, h))  # orignal shape ~ HxW
+
+            # ! problem with this one
+            thresholded_img = self.apply_thresholding(mask.copy())
+            print("Thresholded img:", thresholded_img.shape)
+            xmin, ymin, xmax, ymax = self.get_bbox(thresholded_img)
+            print("Bounding Box:", xmin, ymin, xmax, ymax)
 
             output_img = (image * mask[..., np.newaxis] + (1 - mask[..., np.newaxis]) * 255).astype(np.uint8)
 
             mask = (mask * 255).astype(np.uint8)
 
-            if visualization:
-                cv2.imwrite(os.path.join(output_dir, 'result_mask_%d.png' % i), mask)
-                cv2.imwrite(os.path.join(output_dir, 'result_%d.png' % i), output_img)
+            print(output_img.shape)
+            print(mask.shape)
 
-            results.append({'mask': mask, 'front': output_img})
+            mask_3channels = cv2.merge((mask, mask, mask))
+            print("Mask 3 channeks:", mask_3channels.shape)
+
+            mat = cv2.bitwise_and(output_img, mask_3channels)
+
+            tmp = cv2.cvtColor(mask_3channels, cv2.COLOR_BGR2GRAY)
+            b, g, r = cv2.split(mat)
+
+            rgba = [b, g, r, tmp]
+            transparent = cv2.merge(rgba, 4)
+
+            # ! anoteher way
+            # res = np.concatenate((img_array * (mask/255), mask[:, :, [0]]), -1)
+            # print(res.shape)
+            # need to convert to RGB
+            res = np.concatenate((output_img[:, :, ::-1], mask_3channels[:, :, [0]]), -1)
+            transparent2 = Image.fromarray(res.astype("uint8"), mode="RGBA")
+
+            crop_output_img = output_img[ymin:ymax, xmin:xmax, :]
+            crop_mask_3channels = mask_3channels[ymin:ymax, xmin:xmax, :]
+            crop_transparent = np.concatenate((crop_output_img[:, :, ::-1], crop_mask_3channels[:, :, [0]]), -1)
+            crop_transparent2 = Image.fromarray(crop_transparent.astype("uint8"), mode="RGBA")
+
+            if visualization:
+                now = datetime.datetime.now()
+                # cv2.imwrite(os.path.join(output_dir, f"result_mask_%d_{now}.png" % i), mask)
+                cv2.imwrite(os.path.join(output_dir, f"result_mask_%d_{now}.png" % i), mask_3channels)
+                # cv2.rectangle(output_img, (xmin, ymin), (xmax, ymax), (0, 255, 255), 2)
+                cv2.imwrite(os.path.join(output_dir, f"result_%d_{now}.png" % i), output_img)
+                # worse then PIL
+                # cv2.imwrite(os.path.join(output_dir, f"result_trans_%d_{now}.png" % i), transparent)
+                # chat luong > tot hon opencv
+                crop_transparent_path = os.path.join(output_dir, f"result_trans_%d_{now}.png" % i)
+                crop_transparent2.save(crop_transparent_path)
+                transparent_path = os.path.join(output_dir, f"result_crop_trans_%d_{now}.png" % i)
+                transparent2.save(transparent_path)
+
+            # return the paths
+            results.append(
+                {"mask": mask, "front": output_img, "trans": transparent_path, "crop_trans": crop_transparent_path}
+            )
 
         return results
+
+    @staticmethod
+    def apply_thresholding(img, threshold=0.9):
+        """ img: HxW """
+        # check the range of pixels
+        # [0,1]
+        if np.max(img) > 1:
+            img = img / 255
+
+        img[img > threshold] = 1
+        img[img <= threshold] = 0
+        return img
+
+    # get the bounding box of the object
+    @staticmethod
+    def get_bbox(img):
+        """ Get the bounding box of object 
+            Binary image H x W, 
+        """
+        if len(img.shape) > 2:
+            print("Shape of image is not valid")
+            return
+
+        # pickup a layer of one channel + black n white image
+        # find the list of x,y
+        x_starts = [
+            np.where(img[i] == 1)[0][0] if len(np.where(img[i] == 1)[0]) != 0 else img.shape[0] + 1
+            for i in range(img.shape[0])
+        ]
+        x_ends = [
+            np.where(img[i] == 1)[0][-1] if len(np.where(img[i] == 1)[0]) != 0 else 0 for i in range(img.shape[0])
+        ]
+        y_starts = [
+            np.where(img.T[i] == 1)[0][0] if len(np.where(img.T[i] == 1)[0]) != 0 else img.T.shape[0] + 1
+            for i in range(img.T.shape[0])
+        ]
+        y_ends = [
+            np.where(img.T[i] == 1)[0][-1] if len(np.where(img.T[i] == 1)[0]) != 0 else 0 for i in range(img.T.shape[0])
+        ]
+
+        # find the min
+        startx = min(x_starts)
+        endx = max(x_ends)
+        starty = min(y_starts)
+        endy = max(y_ends)
+
+        # tuple
+        start = (startx, starty)
+        # tuple
+        end = (endx, endy)
+
+        return startx, starty, endx, endy
+
+    def predict(self):
+        outputs = []
+        for data in self.input_datas:
+            # convert numpy array to torch Float Tensor
+            data = torch.from_numpy(data)
+            # change the data type
+            data = data.type(torch.FloatTensor)
+            if torch.cuda.is_available():
+                data = Variable(data.cuda())
+            else:
+                data = Variable(data)
+
+            # ! replace the paddle with torch
+            d1, d2, d3, d4, d5, d6, d7 = self.model(data)
+
+            # CUDA tensor -> host memory first -> convert to numpy
+            outputs.append(d1.detach().cpu().numpy())
+
+            # model results
+            outputs = np.concatenate(outputs, 0)
+
+            return outputs
